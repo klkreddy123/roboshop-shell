c7f208324857b62b9e3874a62ef9b23ea9c7283f
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-26 17:59:27 +0530

    createec2.sh

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,8 +3,8 @@
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0189d4870cc65fd55
-DOMAIN_NAME=joindevops.online
+SECURITY_GROUP_ID=sg-0a609e2e2cba7da31
+DOMAIN_NAME=kautomation.online
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -17,6 +17,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID 
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 done
