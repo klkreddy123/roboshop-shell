ae36f26a72adebd933af34fb1b92bc9f38a91971
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-26 16:33:51 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -60,7 +60,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing dependencies"
 
 # give full path of catalogue.service because we are inside /app
-cp root/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
 VALIDATE $? "copying catalogue.service"
 
