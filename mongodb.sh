b06c61c3087fac9b0978e6fee74a08cca1a4b400
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-26 13:32:21 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
-USERID=$(id -u)
-LOGSDIR=/tmp
 DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
-RED="\e[31m"
-NOCOLOR="\e[0m"
-GREEN="\e[32m"
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
 
 VALIDATE(){
-
-if [ $1 -ne 0 ]
-then
-    echo -e "$2 is $RED failure $NOCOLOR"
-    exit 1
-else
-    echo -e "$2 is $GREEN success $NOCOLOR"
-fi
-
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo -e "$RED Failure: Logged in user is not root user $NOCOLOR"
-    exit 1
-else
-    echo -e "$GREEN Success: Logged in user is root user $NOCOLOR"
-fi
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
-VALIDATE $? "Copied MongoDB repo in to yum.repos.d"
+VALIDATE $? "Copied MongoDB repo into yum.repos.d"
 
 yum install mongodb-org -y &>> $LOGFILE
 
@@ -39,16 +38,16 @@ VALIDATE $? "Installation of MongoDB"
 
 systemctl enable mongod &>> $LOGFILE
 
-VALIDATE $? "Enabling of MongoDB"
+VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod &>> $LOGFILE
 
-VALIDATE $? "starting MongoDB"
+VALIDATE $? "Starting MongoDB"
 
-sed -i 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
 
-VALIDATE $? "Editing MongoDB conf" 
+VALIDATE $? "Edited MongoDB conf"
 
 systemctl restart mongod &>> $LOGFILE
 
-VALIDATE $? "Restarting MongoDB"
+VALIDATE $? "Restarting MonogoDB"
